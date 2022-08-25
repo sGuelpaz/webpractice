e670802b55d229f88dd355bc93c8f427ccb91e71
sGuelpaz <jhon.guelpaz@misena.edu.co>
2022-08-25 11:19:23 -0500

    Reportes excel sin fecha

--- WebPractice/WebPractice/Controllers/HomeController.cs
+++ WebPractice/WebPractice/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -7,20 +8,62 @@ using System.Linq;
 using System.Threading.Tasks;
 using WebPractice.Models;
 
+using System.Data;
+using System.Data.SqlClient;
+
+using ClosedXML.Excel;
+using System.IO;
+
 namespace WebPractice.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly string DefaultConnection;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IConfiguration config)
         {
-            _logger = logger;
+            DefaultConnection = config.GetConnectionString("DefaultConnection");
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Exportar_excel()
+        {
+            DataTable tabla_usuario = new DataTable();
+
+            using (var conexion = new SqlConnection(DefaultConnection))
+            {
+                conexion.Open();
+                using (var adapter = new SqlDataAdapter())
+                {
+                    adapter.SelectCommand = new SqlCommand("sp_reporte_usuario", conexion);
+                    adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                    adapter.Fill(tabla_usuario);
+                }
+            }
+
+            using (var libro = new XLWorkbook())
+            {
+                tabla_usuario.TableName = "Usuarios";
+                var hoja = libro.Worksheets.Add(tabla_usuario);
+                hoja.ColumnsUsed().AdjustToContents();
+
+                using (var memoria = new MemoryStream())
+                {
+                    libro.SaveAs(memoria);
+
+                    var nombreExcel = string.Concat("Reporte usuario ", DateTime.Now.ToString(), ".xlsx");
+
+                    return File(memoria.ToArray(), "application/vdn.openxmlformats-officedocument.spreadsheetml.sheet", nombreExcel);
+                }
+
+            }
+
+            //return View();
         }
 
         public IActionResult Privacy()
---
+++ WebPractice/WebPractice/Models/ReporteUsuarios.cs
@@ -0,0 +1,17 @@
+﻿namespace WebPractice.Models
+{
+    public class ReporteUsuarios
+    {
+        // Nombre,Apellido,Telefono,Correo,Direccion,Pais
+        public string Documento { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string Direccion { get; set; }
+        public string Celular { get; set; }
+        public string Genero { get; set; }
+        public string Deporte { get; set; }
+        public string Trabaja { get; set; }
+        public string Sueldo { get; set; }
+        public string Estado { get; set; }
+    }
+}
